a4324bcb64e613e66c27f5b4351c13055bb5feef
Harshal Tripathi <harshal.free@gmail.com>
2023-01-09 23:24:38 -0500

    Formatting

--- src/components/GameSwitcher.jsx
+++ src/components/GameSwitcher.jsx
@@ -9,7 +9,6 @@ class GameSwitcher extends Component {
     };
 
     clickBtn = e => {
-        console.log(e.target.value);
         this.setState({
             renderView: e.target.value
         });
@@ -19,8 +18,10 @@ class GameSwitcher extends Component {
         return (
             <div className="switcher">
                 <nav className="navigation">
-                    <button value={1} onClick={this.clickBtn} className="button">Pi Game</button>
-                    <button value={2} onClick={this.clickBtn} className="button">Snakes & Ladders Game</button>
+                    <button value={1} onClick={this.clickBtn} disabled={this.state.renderView == 1 ? 'true'.length > 0 : false} 
+                    className={this.state.renderView == 1 ? 'button active' : 'button disabled'}>Pi Game</button>
+                    <button value={2} onClick={this.clickBtn} disabled={this.state.renderView == 2 ? 'true'.length > 0 : false}
+                    className={this.state.renderView == 2 ? 'button active' : 'button disabled'}>Snakes & Ladders Game</button>
                 </nav>
                 <div className="gameboard">
                     {/* {this.state.renderView == 1 && <PiGame className={`${this.state.renderView == 1 ? "show" : "noshow"}`}/>}
